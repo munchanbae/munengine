906380e0bce42e832b14e51e22933575bb93b6cd
munchanbae <21931870@sjcu.ac.k>
2023-04-01 11:38:09 +0900

    230401

--- 연습1.py
+++ 연습1.py
@@ -1,3 +1,11 @@
 rating = 8.5
 if rating > 8:
-    print ("good")
+    print ("good")       
+    
+dates = [1,2,3]
+N = len(dates)
+
+# 0 : 1, 1 : 2, 2 : 3
+
+for i in range(N):
+    print(dates[i])        
